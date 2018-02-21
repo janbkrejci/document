069f12465b5ac73b73f809b48d4cad06f48e9beb
Jan B. Krejčí <janbkrejci@gmail.com>
2018-02-21 12:43:31 +0000

    version 1.0

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Document Indexation using external tools',
+    'version': '1.0',
+    'category': 'Document Management',
+    'description': """
+Document indexation using external tools
+========================================
+
+Prerequisities: installed packages libreoffice and poppler-utils
+
+Indexes .doc, .docx, .odp, .ods, .odt, .wps, .rtf, .ppt, .pdf, .xls, .xlsx
+
+At first, translates the attachment using soffice --headless to .pdf,
+then uses pdftotext from poppler-utils.
+
+Please have a look into /tmp directory from time to time, since there can
+be some orphan files to delete. These files can stay in /tmp if the
+conversion crashes.
+
+""",
+    'depends': [],
+    'data': [],
+    'installable': True,
+}
