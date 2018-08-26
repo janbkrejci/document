47c8cd5b323aed922be5e0f899c1f497a95af9f9
Jan B. Krejčí <janbkrejci@gmail.com>
2018-08-26 17:28:12 +0200

    Update __manifest__.py

--- document-indexing/__manifest__.py
+++ document-indexing/__manifest__.py
@@ -48,6 +48,30 @@ Please take a look into /tmp directory from time to time, since there can
 stil be some orphan files that you may want delete manually. These files can remain
 in /tmp if the conversion crashes for some reason.
 
+## How to install under docker image with odoo11
+
+1) download and run odoo image
+2) docker exec -it -u root odoo11 /bin/bash
+3) cd /var/lib/odoo/addons/11.0
+4) apt-get update
+5) apt-get install git libreoffice poppler-utils
+6) git clone https://github.com/janbkrejci/document-indexing
+7) in Odoo, update modules list and install the module
+8) done - test it
+
+## How to install to on-premise Odoo installation under linux
+
+1) apt-get install git libreoffice poppler-utils
+2) cd addons path
+3) git clone https://github.com/janbkrejci/document-indexing
+4) in Odoo, update modules list and install the module
+5) done - test it
+
+## How to install to on-premise Odoo installation under Windows
+
+..no idea, just try to make sure that you have everything needed
+to be able to run "soffice" and "pdftotext" commands from command line.
+
 """,
     'depends': ['document'],
     'data': [],
